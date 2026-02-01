6614783e7aa740eaafdf37ba4328a442ea703c6b
sachinks <sachin.kolige@gmail.com>
2026-02-01 19:40:13 +0000

    Bind Flask to 0.0.0.0 and use Render PORT

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +7,5 @@ def home():
     return "Hello! This Python app is running on Render 🚀"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
